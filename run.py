09538c55301b91856b5ff36acd27e21de4588673
Ophuscado <postbox@ophuscado.com>
2022-11-17 01:40:49 +0100

    Add copyright to images

--- run.py
+++ run.py
@@ -12,35 +12,41 @@ import random
 
 from dotenv import load_dotenv
 from instagrapi import Client, exceptions
-from PIL import Image
+from PIL import Image, ImageDraw
 
 # %%
 load_dotenv()
 
 IMAGES_PATH = os.environ["IMAGES_PATH"]
 IMAGES_CAPTION = os.environ["IMAGES_CAPTION"]
-USERNAME = os.environ["USERNAME"]
+IMAGES_COPYRIGHT = os.environ["IMAGES_COPYRIGHT"]
 PASSWORD = os.environ["PASSWORD"]
+USERNAME = os.environ["USERNAME"]
 
 
 # %%
 def main():
     """Main function."""
-    cl = Client()
-    cl.login(USERNAME, PASSWORD)
-
     images = os.listdir(IMAGES_PATH)
     image = random.choice(images)
     image_path_png = os.path.join(IMAGES_PATH, image)
-
-    # From PNG to JPG
     image = Image.open(image_path_png)
-    image = image.convert("RGB")
+
+    draw = ImageDraw.Draw(image)
+    draw.text(
+        (image.width / 2 - len(IMAGES_COPYRIGHT) * 3, image.height - 16),
+        IMAGES_COPYRIGHT,
+        fill=(255, 255, 255, 128),
+        align="center",
+        anchor="mm",
+    )
     image_path_jpg = image_path_png.replace(".png", ".jpg")
     image.save(image_path_jpg, "JPEG")
 
     try:
-        cl.photo_upload(
+        ig = Client()
+        ig.login(USERNAME, PASSWORD)
+        ig.photo_upload(
             image_path_jpg,
             IMAGES_CAPTION,
         )
@@ -48,7 +54,6 @@ def main():
         os.remove(image_path_png)
     except exceptions.PhotoNotUpload:
         os.remove(image_path_jpg)
-        print("Photo not uploaded.")
 
 
 # %%
